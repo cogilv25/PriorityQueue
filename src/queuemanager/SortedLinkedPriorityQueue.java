65dcdbda9c5b593d11e10ee918c450cec1e956d6
cogilv25 <calumlindsay.434c@gmail.com>
2023-03-10 17:01:50 +0000

    Initial SLPQ implementation complete

--- src/queuemanager/SortedLinkedPriorityQueue.java
+++ src/queuemanager/SortedLinkedPriorityQueue.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package queuemanager;
 
 /**
@@ -13,25 +9,104 @@ package queuemanager;
  * @author Calum Lindsay
  */
 public class SortedLinkedPriorityQueue<T> implements PriorityQueue<T> {
+    /**
+     * The head or start of the linked list.
+     */
+    private Node<PriorityItem<T>> head = null;
 
+    /**
+     * The number of items held in the queue.
+     */
+    private int count = 0;
+    
+    
+    /* These functions inherit their JavaDoc comments from PriorityQueue. */
+    
     @Override
-    public void add(T item, int priority) throws QueueOverflowException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void add(T item, int priority) throws QueueOverflowException
+    {
+        /* Create a new Node containing a new PriorityItem */
+        Node<PriorityItem<T>> newNode = new Node<>(new PriorityItem<>(item,priority));
+        
+        /* If the queue is empty insert at head and return. */
+        if(isEmpty())
+        {
+            head = newNode;
+            count++;
+            return;
+        }
+        
+        /* Variables to track insertion point */
+        Node<PriorityItem<T>> insertAfter = null;
+        Node<PriorityItem<T>> insertBefore = head;
+        
+        /* Find the insertion point. */
+        while(insertBefore != null)
+        {
+            if(priority > insertBefore.getValue().getPriority())
+                break;
+
+            insertAfter = insertBefore;
+            insertBefore = insertBefore.getNext();
+        }
+        
+        
+        /* Insert it into the linked list at the insertion point and update head
+         * if we replaced it. */
+        newNode.setNext(insertBefore);
+        if(insertAfter == null)
+            head = newNode;
+        else
+            insertAfter.setNext(newNode);
+        
+        count++;
     }
 
+    
     @Override
-    public T head() throws QueueUnderflowException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public T head() throws QueueUnderflowException
+    {
+        if(isEmpty())
+            throw new QueueUnderflowException();
+        
+        return head.getValue().getItem();
     }
 
+    
     @Override
-    public void remove() throws QueueUnderflowException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public boolean isEmpty() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void remove() throws QueueUnderflowException
+    {
+        if(isEmpty())
+            throw new QueueUnderflowException();
+        
+        /* We know there is at least one item in the list. */
+        count--;
+        
+        /* Move the head reference forward one place in the list. */
+        if(count > 0)
+            head = head.getNext();
     }
     
+    
+    @Override
+    public String toString() {
+        /* Construct a comma delimited list of items in the queue. */
+        String result = "[";
+        Node<PriorityItem<T>> current = head;
+        for (int i = 0; i < count; i++) {
+            if (i > 0) {
+                result = result + ", ";
+            }
+            result = result + current.getValue();
+            current = current.getNext();
+        }
+        result = result + "]";
+        return result;
+    }
+    
+    
+    @Override
+    public boolean isEmpty() {
+        return (count < 1);
+    }
 }
